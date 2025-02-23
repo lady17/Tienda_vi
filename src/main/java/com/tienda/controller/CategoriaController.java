f2bec951fa3715382f5727e462235f86e32eaacf
leidy <leidy@LadySalgado>
2025-02-23 16:58:11 -0600

    Semana 5: Finalizado el CRUD de categoria

--- src/main/java/com/tienda/controller/CategoriaController.java
+++ src/main/java/com/tienda/controller/CategoriaController.java
@@ -1,12 +1,16 @@
 
 package com.tienda.controller;
 
+import com.tienda.domain.Categoria;
 import com.tienda.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 /*Todo lo que el usuario llame como Categoria va a entrar en esta clase */
 
 @Controller
@@ -17,13 +21,30 @@ public class CategoriaController {
     private CategoriaService categoriaService;
     
     @GetMapping ("/listado")
-    public String listado (Model model) {
-        
-        
+    public String listado (Model model) { 
         var lista = categoriaService.getCategorias(false);
         model.addAttribute("categorias", lista);
-        
-        
+        model.addAttribute("totalCategorias", lista.size()); 
         return "/categoria/listado";
+    }
+    
+    @GetMapping ("/eliminar/{idCategoria}")
+    public String eliminar (Categoria categoria) { 
+        categoriaService.delete(categoria);
+        return "return:/categoria/listado";
+    }
+    
+    @GetMapping ("/modificar/{idCategoria}")
+    public String modificar (Categoria categoria, Model model) { 
+        categoria=categoriaService.getCategoria(categoria);
+        model.addAttribute("categoria", categoria);
+        return "/categoria/modifica";
+    }
+    
+    @PostMapping("/guardar")
+    public String guardar(Categoria categoria,
+            @RequestParam("imagenFile") MultipartFile imagenFile) {
+        categoriaService.save(categoria);
+        return "redirect:/categoria/listado";
     }
 }
--- src/main/java/com/tienda/service/CategoriaService.java
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -17,9 +17,33 @@ public class CategoriaService {
     @Transactional(readOnly=true)
     public List<Categoria> getCategorias(boolean activos) {
         var lista = categoriaRepository.findAll();
-        
+        //acá falta un codigo
         return lista;
         
         
     }
+    //Se crean los metodos para un CRUD CREATE READ UPDATE DELETE
+    
+    @Transactional(readOnly=true)
+    public Categoria getCategoria(Categoria categoria) {
+        categoria = categoriaRepository.findById(categoria.getIdCategoria()).orElse(null);
+        return categoria;
+    }
+    
+    @Transactional
+    public void delete (Categoria categoria) {
+        //Elimina el registroo que tiene el idCategoria pasado en el objeto categoria
+        categoriaRepository.delete(categoria);
+
+    }
+    
+     @Transactional
+    public void save (Categoria categoria) {
+        //Si el ID categoria tiene un valor se actualiza
+        //Si el ID categoria no tiene valor, se agrega
+        categoriaRepository.save(categoria);
+
+    }
+
+        
 }
